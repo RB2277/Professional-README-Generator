16729b27e4ffebb407d9fe6d4bb89fe925d6d685
RB <jsriley24505@gmail.com>
2024-01-23 13:27:24 -0600

    added questions to the const questions array

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your project title?", "Provide a description of your project", "Provide any installation instructions", "Provide any usage information",
+"Provide any contribution guidelines", "Provide any test instructions", "Which license will you use?", "What is your github username?", "What is your email address?" ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
